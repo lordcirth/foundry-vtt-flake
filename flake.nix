b1f126e32f19ce5b9e94fee7c409c59e56dfaddf
Nathan Fish <lordcirth@gmail.com>
2020-11-13 00:26:07 -0500

    try super for callPackage

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = { self, nixpkgs }: rec {
     overlay = self: super: {
-      foundry-vtt = nixpkgs.pkg.callPackage ./foundry-vtt-headless.nix { };
+      foundry-vtt = super.nixpkgs.pkg.callPackage ./foundry-vtt-headless.nix { };
     };
   };
 }
