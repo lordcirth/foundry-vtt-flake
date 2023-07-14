a622df866e2ac51e03917237190789323b3b25a1
Nathan Fish <lordcirth@gmail.com>
2023-07-14 15:19:44 -0400

    update to node 16

--- foundry-vtt-service.nix
+++ foundry-vtt-service.nix
@@ -32,7 +32,7 @@ in {
         Environment="HOME=/var/lib/foundry-vtt";
       };
       script =
-        "${pkgs.nodejs-14_x}/bin/node ${foundry-vtt}/resources/app/main.js --dataPath=/var/lib/foundry-vtt";
+        "${pkgs.nodejs-16_x}/bin/node ${foundry-vtt}/resources/app/main.js --dataPath=/var/lib/foundry-vtt";
     };
   };
 }
